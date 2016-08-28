90f57b7823fdfba9b044c478f2dcbe611ecf0572
Andrea Zucchini <azga84@gmail.com>
2016-08-28 18:53:10 +0200

    Create movement component

--- src/app/movement/movement.component.ts
+++ src/app/movement/movement.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 import Category from './model/Category';
 import {Movement} from './model/Movement';
+import {TypesOfMovements} from './enums/typesOfMovements';
 
 
 @Component({
@@ -18,30 +19,59 @@ export class MovementComponent implements OnInit {
   public order: number;
   public income: number;
   public expense: number;
+  public balance: number;
 
-  constructor({
-    incomeCategories = Category.getIncomeCategories(),
-    expenditureCategories = Category.getExpenditureCategories(),
-  }: {
-    incomeCategories?: string[],
-    expenditureCategories?: string[]
-  } = {}) {
-    this.incomeCategories = incomeCategories;
-    this.expenditureCategories = expenditureCategories;
-  }
-
-  ngOnInit() {
+  ngOnInit(): any {
+    this.incomeCategories = Category.getIncomeCategories();
+    this.expenditureCategories = Category.getExpenditureCategories();
+    this.income = 0;
+    this.expense = 0;
     this.movement = new Movement();
+    this.movements = [];
   }
 
-  save() {
-    if (this.isIncome()) {
-      this.income += this.movement.getAmount();
+  save(): void {
+    if (this.movement.isIncome()) {
+      this.income += this.movement.amount;
+    } else {
+      this.expense += this.movement.amount;
     }
+    this.balance = this.calculateBalance();
+    this.movements.push(new Movement({
+      type: this.movement.type,
+      id: this.movement.id,
+      category: this.movement.category,
+      date: this.movement.date,
+      amount: this.movement.amount
+    }));
   }
 
+  orderBy(field: string): void {
+    this.order = -1 * this.order;
+    this.movements.sort((a, b) => a[field] < b[field] ? this.order : -1 * this.order);
+  }
 
-  private isIncome() {
+  getDate(stringDate: string): Date {
+    return new Date(stringDate);
+  }
+
+  movementIsIncome() {
     return this.movement.isIncome();
+  }
+
+  getEntryValue(): number {
+    return TypesOfMovements.Entry;
+  }
+
+  getExpenseValue(): number {
+    return TypesOfMovements.Expense;
+  }
+
+  onChangeType(event): void {
+    this.movement.type = parseInt(event.currentTarget.value, 10);
+  }
+
+  private calculateBalance() {
+    return this.income - this.expense;
   }
 }
