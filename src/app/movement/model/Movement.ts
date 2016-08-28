3fb9c82ebe1874e136a7df1061aa78fd8ed68042
Andrea Zucchini <azga84@gmail.com>
2016-08-28 18:52:30 +0200

    Create Movement model

--- src/app/movement/model/Movement.ts
+++ src/app/movement/model/Movement.ts
@@ -1,3 +1,83 @@
-/**
- * Created by Andrea on 27/08/2016.
- */
+import {TypesOfMovements} from '../enums/typesOfMovements';
+
+interface IMovement {
+  id: string;
+  type: TypesOfMovements;
+  category: string;
+  date: Date;
+  amount: number;
+}
+
+export class Movement implements IMovement {
+  private _id: string;
+  private _type: TypesOfMovements;
+  private _category: string;
+  private _date: Date;
+  private _amount: number;
+
+
+  constructor({
+    id = new Date().toDateString(),
+    type = TypesOfMovements.Entry,
+    category = 'Nómina',
+    date = new Date(),
+    amount = 0
+  }: {
+    id?: string;
+    type?: TypesOfMovements;
+    category?: string;
+    date?: Date;
+    amount?: number
+  } = {}) {
+    this._id = id;
+    this._type = type;
+    this._category = category;
+    this._date = date;
+    this._amount = amount;
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  set id(id: string) {
+    this._id = id;
+  }
+
+  get type(): TypesOfMovements {
+    return this._type;
+  }
+
+  set type(type: TypesOfMovements) {
+    this._type = type;
+  }
+
+  get category(): string {
+    return this._category;
+  }
+
+  set category(category: string) {
+    this._category = category;
+  }
+
+  get date(): Date {
+    return this._date;
+  }
+
+  set date(date: Date) {
+    this._date = date;
+  }
+
+  get amount(): number {
+    return this._amount;
+  }
+
+  set amount(amount: number) {
+    this._amount = amount;
+  }
+
+  isIncome(): boolean {
+    return this.type === TypesOfMovements.Entry;
+  }
+
+}
