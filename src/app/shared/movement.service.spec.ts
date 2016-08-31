525381f6b6f79560cbb4caa44cd69c93f5cba234
Andrea Zucchini <azga84@gmail.com>
2016-08-31 08:28:36 +0200

    Create MovementService

---
+++ src/app/shared/movement.service.spec.ts
@@ -0,0 +1,16 @@
+/* tslint:disable:no-unused-variable */
+
+import { addProviders, async, inject } from '@angular/core/testing';
+import { MovementService } from './movement.service';
+
+describe('Service: Movement', () => {
+  beforeEach(() => {
+    addProviders([MovementService]);
+  });
+
+  it('should ...',
+    inject([MovementService],
+      (service: MovementService) => {
+        expect(service).toBeTruthy();
+      }));
+});
