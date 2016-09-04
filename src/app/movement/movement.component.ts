c2c7956ad0101605de814255a1a4420ba28f14f4
Andrea Zucchini <azga84@gmail.com>
2016-09-04 13:10:01 +0200

    Clear the editing logic of movement

--- src/app/movement/movement.component.ts
+++ src/app/movement/movement.component.ts
@@ -1,44 +1,38 @@
 import {Component, OnInit, provide} from '@angular/core';
 import {TypesOfMovements} from '../shared/enums/typesOfMovements';
 import {MovementService, MovementModel} from '../shared';
-
+import {EditorComponent} from './editor';
+import {IAccountingCost} from '../shared/intefaces/IAccountingCost';
 
 @Component({
   moduleId: module.id,
   selector: 'app-movement',
   templateUrl: 'movement.component.html',
   styleUrls: ['movement.component.css'],
+  directives: [EditorComponent],
   providers: [
     provide(MovementService, {useClass: MovementService}),
     provide(MovementModel, {useFactory: () => MovementModel.create()})
   ]
 })
 export class MovementComponent implements OnInit {
-  public incomeCategories: string[];
-  public expenditureCategories: string[];
   public movements: MovementModel[];
   public order: number;
-  public income: number;
-  public expense: number;
-  public balance: number;
+  public typeEntry: TypesOfMovements;
+  public typeExpense: TypesOfMovements;
+  public accountingCost: IAccountingCost;
+
 
   constructor(private movementService: MovementService, public movement: MovementModel) {
 
   }
 
   ngOnInit(): any {
-    this.incomeCategories = this.movementService.incomeCategories;
-    this.expenditureCategories = this.movementService.expenditureCategories;
     this.movements = this.movementService.movements;
     this.order = MovementService.ORDER.ASC;
-  }
-
-  save(): void {
-    this.movementService.registerMovement(this.movement);
-    this.income = this.movementService.income;
-    this.expense = this.movementService.expense;
-    this.balance = this.movementService.balance;
-    this.movement = MovementModel.create();
+    this.typeEntry = this.movementService.typeEntry;
+    this.typeExpense = this.movementService.typeExpense;
+    this.accountingCost = this.movementService.accountingCost;
   }
 
   orderBy(field: string): void {
@@ -50,28 +44,8 @@ export class MovementComponent implements OnInit {
     return new Date(stringDate);
   }
 
-  getEntryValue(): number {
-    return TypesOfMovements.Entry;
-  }
-
-  getExpenseValue(): number {
-    return TypesOfMovements.Expense;
-  }
-
   getTypeText(type: number): string {
-    return MovementModel.getTypeText(type);
-  }
-
-  getEntryText(): string {
-    return this.getTypeText(TypesOfMovements.Entry);
-  }
-
-  getExpenseText(): string {
-    return this.getTypeText(TypesOfMovements.Expense);
-  }
-
-  onChangeType(event): void {
-    this.movement.type = parseInt(event.currentTarget.value, 10);
+    return MovementService.getTypeText(type);
   }
 
   isNegativeBalance(): boolean {
