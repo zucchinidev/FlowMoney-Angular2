66c91478b8f9052573526ff93149d5b4a33ef546
Andrea Zucchini <azga84@gmail.com>
2016-09-04 13:34:36 +0200

    inject service in editor component test

--- src/app/movement/editor/editor.component.spec.ts
+++ src/app/movement/editor/editor.component.spec.ts
@@ -4,10 +4,18 @@ import { By }           from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 import { addProviders, async, inject } from '@angular/core/testing';
 import { EditorComponent } from './editor.component';
+import {MovementService} from '../../shared/movement.service';
 
 describe('Component: Editor', () => {
+  let service;
+  beforeEach(() => {
+    addProviders([MovementService]);
+    inject([MovementService], (movementService: MovementService) => {
+      service = movementService;
+    });
+  });
   it('should create an instance', () => {
-    let component = new EditorComponent();
+    let component = new EditorComponent(service);
     expect(component).toBeTruthy();
   });
 });
